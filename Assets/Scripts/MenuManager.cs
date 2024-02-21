0a859cedfb00def52b76314359a3ca3acc96ae0d
Eduland1000 <153714961+Eduland1000@users.noreply.github.com>
2024-02-21 11:22:37 +0100

    menu comite

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagament;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+  public void LoadFirstLevel()
+  {
+    
+  }
+
+}
